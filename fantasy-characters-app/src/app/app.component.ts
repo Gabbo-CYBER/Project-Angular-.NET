b39b1938ffaf95191e955c53a64e153f3fa05c37
Gabbo-CYBER <gabbo.lamonica29@gmail.com>
2024-12-13 18:10:55 +0100

    front-end

---
+++ fantasy-characters-app/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'fantasy-characters-app';
+
+  // Puoi aggiungere altre proprietà o metodi qui
+}
+
+
+
+
+
+
+
+
